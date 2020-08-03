1dd06ac687b9a842dad3837fdaaca177f0577bc5
Zhao Xunan <zhaoxn.bri@chinatelecom.cn>
2020-08-03 13:51:22 +0800

    basic

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import Koa from 'koa';
 import cors from '@koa/cors';
 import bodyParser from 'koa-bodyparser';
+import router from './routes';
+import { logger } from './logger';
+import { createConnection } from 'typeorm';
+import 'reflect-metadata';
 
 // 初始化 Koa 应用实例
 const app = new Koa();
@@ -8,11 +12,13 @@ const app = new Koa();
 // 注册中间件
 app.use(cors());
 app.use(bodyParser());
+app.use(logger());
 
 // 响应用户请求
-app.use((ctx) => {
-  ctx.body = 'Hello Koa';
-});
+// app.use((ctx) => {
+//   ctx.body = 'Hello Koa';
+// });
+app.use(router.routes()).use(router.allowedMethods());
 
 // 运行服务器
 app.listen(3000);
